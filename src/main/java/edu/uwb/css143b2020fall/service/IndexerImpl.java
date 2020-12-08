100052255ebb2dea7e4ba3a455de792bab21b05c
Samantha-Pierce <spapierce@gmail.com>
2020-12-08 04:10:08 -0800

    built index to store first document

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -12,7 +12,27 @@ public class IndexerImpl implements Indexer {
     public Map<String, List<List<Integer>>> index(List<String> docs) {
         Map<String, List<List<Integer>>> indexes = new HashMap<>();
         // add your code
-        
+        //List<List<Integer>> docPos = new ArrayList<>();
+        List<Integer> wordPos;
+        String keyWord;
+        Map<String, List> oneDoc = new HashMap<>();
+        // gets the document
+        for (int docsIndex = 0; docsIndex < docs.size(); docsIndex++) {
+            String[] getWords = docs.get(docsIndex).trim().split("\\s");
+            // gets each word in the document and builds index for current document
+            for (int wordIndex = 0; wordIndex < getWords.length; wordIndex++) {
+                keyWord = getWords[wordIndex];
+                wordPos = oneDoc.get(keyWord);
+                if (wordPos == null) {
+                    wordPos = new ArrayList<>();
+                }
+                wordPos.add(wordIndex);
+                oneDoc.put(keyWord, wordPos);
+            }
+            //docPos.add(wordPos);
+            //oneDoc.put(keyWord, docPos);
+            //indexes.putAll(oneDoc);
+        }
         return indexes;
     }
 }
